c6f8552e0cfaf7d86b2b9385ecc98b80557d21a6
user <user@user-pc>
2018-08-16 17:32:24 +0900

    두번째 가즈아

---
+++ src/main/java/com/mybottle/webapp/controller/AlarmController.java
@@ -0,0 +1,12 @@
+package com.mybottle.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AlarmController {
+	@RequestMapping("/client/alarm/reg")
+	public String aaaaaaa() {
+				
+		return "/WEB-INF/views/client/alarm/reg.jsp"; }
+}
